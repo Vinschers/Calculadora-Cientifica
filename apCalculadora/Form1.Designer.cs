434321006b64a51864b5273d8147bb9ba2ea6245
MillerGS03 <gumiller2003@gmail.com>
2019-04-18 14:00:15 -0300

    Botões responsivos

--- apCalculadora/Form1.Designer.cs
+++ apCalculadora/Form1.Designer.cs
@@ -80,6 +80,7 @@
             // 
             // btnSqrt
             // 
+            this.btnSqrt.Font = new System.Drawing.Font("Arial", 20F);
             this.btnSqrt.Location = new System.Drawing.Point(12, 136);
             this.btnSqrt.Name = "btnSqrt";
             this.btnSqrt.Size = new System.Drawing.Size(100, 100);
@@ -90,6 +91,7 @@
             // 
             // btnCE
             // 
+            this.btnCE.Font = new System.Drawing.Font("Arial", 20F);
             this.btnCE.Location = new System.Drawing.Point(118, 136);
             this.btnCE.Name = "btnCE";
             this.btnCE.Size = new System.Drawing.Size(100, 100);
@@ -100,6 +102,7 @@
             // 
             // btnC
             // 
+            this.btnC.Font = new System.Drawing.Font("Arial", 20F);
             this.btnC.Location = new System.Drawing.Point(224, 136);
             this.btnC.Name = "btnC";
             this.btnC.Size = new System.Drawing.Size(100, 100);
@@ -110,7 +113,7 @@
             // 
             // btnApagar
             // 
-            this.btnApagar.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagar.Font = new System.Drawing.Font("Arial", 25F);
             this.btnApagar.Location = new System.Drawing.Point(330, 136);
             this.btnApagar.Name = "btnApagar";
             this.btnApagar.Size = new System.Drawing.Size(100, 100);
@@ -121,6 +124,7 @@
             // 
             // btnDividir
             // 
+            this.btnDividir.Font = new System.Drawing.Font("Arial", 20F);
             this.btnDividir.Location = new System.Drawing.Point(436, 136);
             this.btnDividir.Name = "btnDividir";
             this.btnDividir.Size = new System.Drawing.Size(100, 100);
@@ -131,6 +135,7 @@
             // 
             // btnLog
             // 
+            this.btnLog.Font = new System.Drawing.Font("Arial", 20F);
             this.btnLog.Location = new System.Drawing.Point(12, 242);
             this.btnLog.Name = "btnLog";
             this.btnLog.Size = new System.Drawing.Size(100, 100);
@@ -141,6 +146,7 @@
             // 
             // btn7
             // 
+            this.btn7.Font = new System.Drawing.Font("Arial", 20F);
             this.btn7.Location = new System.Drawing.Point(118, 242);
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(100, 100);
@@ -151,6 +157,7 @@
             // 
             // btn8
             // 
+            this.btn8.Font = new System.Drawing.Font("Arial", 20F);
             this.btn8.Location = new System.Drawing.Point(224, 242);
             this.btn8.Name = "btn8";
             this.btn8.Size = new System.Drawing.Size(100, 100);
@@ -161,6 +168,7 @@
             // 
             // btn9
             // 
+            this.btn9.Font = new System.Drawing.Font("Arial", 20F);
             this.btn9.Location = new System.Drawing.Point(330, 242);
             this.btn9.Name = "btn9";
             this.btn9.Size = new System.Drawing.Size(100, 100);
@@ -171,6 +179,7 @@
             // 
             // btnMultiplicar
             // 
+            this.btnMultiplicar.Font = new System.Drawing.Font("Arial", 20F);
             this.btnMultiplicar.Location = new System.Drawing.Point(436, 242);
             this.btnMultiplicar.Name = "btnMultiplicar";
             this.btnMultiplicar.Size = new System.Drawing.Size(100, 100);
@@ -181,6 +190,7 @@
             // 
             // btnFatorial
             // 
+            this.btnFatorial.Font = new System.Drawing.Font("Arial", 20F);
             this.btnFatorial.Location = new System.Drawing.Point(12, 348);
             this.btnFatorial.Name = "btnFatorial";
             this.btnFatorial.Size = new System.Drawing.Size(100, 100);
@@ -191,6 +201,7 @@
             // 
             // btn4
             // 
+            this.btn4.Font = new System.Drawing.Font("Arial", 20F);
             this.btn4.Location = new System.Drawing.Point(118, 348);
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(100, 100);
@@ -201,6 +212,7 @@
             // 
             // btn5
             // 
+            this.btn5.Font = new System.Drawing.Font("Arial", 20F);
             this.btn5.Location = new System.Drawing.Point(224, 348);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(100, 100);
@@ -211,6 +223,7 @@
             // 
             // btn6
             // 
+            this.btn6.Font = new System.Drawing.Font("Arial", 20F);
             this.btn6.Location = new System.Drawing.Point(330, 348);
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(100, 100);
@@ -221,6 +234,7 @@
             // 
             // btnMenos
             // 
+            this.btnMenos.Font = new System.Drawing.Font("Arial", 20F);
             this.btnMenos.Location = new System.Drawing.Point(436, 348);
             this.btnMenos.Name = "btnMenos";
             this.btnMenos.Size = new System.Drawing.Size(100, 100);
@@ -231,6 +245,7 @@
             // 
             // btnElevado
             // 
+            this.btnElevado.Font = new System.Drawing.Font("Arial", 20F);
             this.btnElevado.Location = new System.Drawing.Point(12, 454);
             this.btnElevado.Name = "btnElevado";
             this.btnElevado.Size = new System.Drawing.Size(100, 100);
@@ -241,6 +256,7 @@
             // 
             // btn1
             // 
+            this.btn1.Font = new System.Drawing.Font("Arial", 20F);
             this.btn1.Location = new System.Drawing.Point(118, 454);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(100, 100);
@@ -251,6 +267,7 @@
             // 
             // btn2
             // 
+            this.btn2.Font = new System.Drawing.Font("Arial", 20F);
             this.btn2.Location = new System.Drawing.Point(224, 454);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(100, 100);
@@ -261,6 +278,7 @@
             // 
             // btn3
             // 
+            this.btn3.Font = new System.Drawing.Font("Arial", 20F);
             this.btn3.Location = new System.Drawing.Point(330, 454);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(100, 100);
@@ -271,6 +289,7 @@
             // 
             // btnMais
             // 
+            this.btnMais.Font = new System.Drawing.Font("Arial", 20F);
             this.btnMais.Location = new System.Drawing.Point(436, 454);
             this.btnMais.Name = "btnMais";
             this.btnMais.Size = new System.Drawing.Size(100, 100);
@@ -281,6 +300,7 @@
             // 
             // btnAbreParenteses
             // 
+            this.btnAbreParenteses.Font = new System.Drawing.Font("Arial", 20F);
             this.btnAbreParenteses.Location = new System.Drawing.Point(12, 560);
             this.btnAbreParenteses.Name = "btnAbreParenteses";
             this.btnAbreParenteses.Size = new System.Drawing.Size(100, 100);
@@ -290,6 +310,7 @@
             // 
             // btnFechaParenteses
             // 
+            this.btnFechaParenteses.Font = new System.Drawing.Font("Arial", 20F);
             this.btnFechaParenteses.Location = new System.Drawing.Point(118, 560);
             this.btnFechaParenteses.Name = "btnFechaParenteses";
             this.btnFechaParenteses.Size = new System.Drawing.Size(100, 100);
@@ -299,6 +320,7 @@
             // 
             // btn0
             // 
+            this.btn0.Font = new System.Drawing.Font("Arial", 20F);
             this.btn0.Location = new System.Drawing.Point(224, 560);
             this.btn0.Name = "btn0";
             this.btn0.Size = new System.Drawing.Size(100, 100);
@@ -309,6 +331,7 @@
             // 
             // btnDecimal
             // 
+            this.btnDecimal.Font = new System.Drawing.Font("Arial", 20F);
             this.btnDecimal.Location = new System.Drawing.Point(330, 560);
             this.btnDecimal.Name = "btnDecimal";
             this.btnDecimal.Size = new System.Drawing.Size(100, 100);
@@ -319,6 +342,7 @@
             // 
             // btnIgual
             // 
+            this.btnIgual.Font = new System.Drawing.Font("Arial", 20F);
             this.btnIgual.Location = new System.Drawing.Point(436, 560);
             this.btnIgual.Name = "btnIgual";
             this.btnIgual.Size = new System.Drawing.Size(100, 100);
@@ -361,6 +385,7 @@
             this.Controls.Add(this.txtVisor);
             this.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.MinimumSize = new System.Drawing.Size(300, 400);
             this.Name = "frmCalculadora";
             this.Text = "Calculadora";
             this.Resize += new System.EventHandler(this.frmCalculadora_Resize);
--- apCalculadora/Form1.cs
+++ apCalculadora/Form1.cs
@@ -12,16 +12,77 @@ namespace apCalculadora
 {
     public partial class frmCalculadora : Form
     {
+        Button[,] botoes = new Button[5, 5];
+
         string operacaoInfixa;
         public frmCalculadora()
         {
             InitializeComponent();
             lblPosFixa.AutoSize = true;
+
+            // Facilita a responsividade
+            ColocarBotoesNaMatriz();
+            
+        }
+        private void ColocarBotoesNaMatriz()
+        {
+            int linha = 4, coluna = 4;
+
+            // Percorre os controles em busca dos botões, que estão em ordem do canto inferior direito até o superior esquerdo
+            foreach (Control ctrl in Controls)
+                if (ctrl.Name.Substring(0, 3) == "btn")
+                {
+                    botoes[linha, coluna] = (Button)ctrl;
+
+                    if (--coluna == -1) // Ao achar um botão, retrocede uma coluna
+                    { // Se acabarem as colunas, retrocede uma linha e volta a coluna até o canto direito
+                        linha--;
+                        coluna = 4;
+
+                        if (linha == -1)
+                            break;
+                    }
+                }
         }
 
         private void frmCalculadora_Resize(object sender, EventArgs e)
         {
             lblPosFixa.MaximumSize = new Size(Width, 0);
+            Reposicionar();       
+        }
+        private void Reposicionar()
+        {
+            // Margem interna (entre os botões)
+            int margem = 3;
+
+            // Cada botão terá 1/5 do tamanho total, desconsiderando espaços externos e margens internas
+            int widthBotoes = (Width - 39 - 8 * margem) / 5; 
+            int heightBotoes = (Height - 183 - 8 * margem) / 5;
+
+            float fontSize;
+
+            // Tamanho da fonte baseado na menor dimensão
+            if (widthBotoes < heightBotoes)
+                fontSize = widthBotoes / 5;
+            else
+                fontSize = heightBotoes / 5;
+
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    botoes[j, i].Width = widthBotoes;
+                    botoes[j, i].Height = heightBotoes;
+
+                    // A localização deve considerar a posição do primeiro e as margens entre os botões
+                    botoes[j, i].Left = 12 + (widthBotoes + 2 * margem) * i;
+                    botoes[j, i].Top = 136 + (heightBotoes + 2 * margem) * j;
+
+                    // btnApagar tem um caracter especial, que deve estar com uma fonte maior
+                    if (botoes[j, i] != btnApagar)
+                        botoes[j, i].Font = new Font(botoes[j, i].Font.FontFamily, fontSize);
+                    else
+                        botoes[j, i].Font = new Font(botoes[j, i].Font.FontFamily, fontSize * 5 / 4);
+                }
         }
 
         private void btnIgual_Click(object sender, EventArgs e) //evento para calcular
